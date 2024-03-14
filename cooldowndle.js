7f597ac01ff4a7726f19c152de542b5bde3aec68
arcan0pulse <niklaslutz09@gmail.com>
2024-03-14 21:54:59 +0100

    finished :)

--- cooldowndle.js
+++ cooldowndle.js
@@ -1,24 +1,54 @@
 let attempts = 0;
-
+let solution = -1;
+//TODO: Jax, Elise, Jayce, Asol, Yuumi, Nidalee, Kled, Fizz
 function intialize() {
     let abilities = document.getElementById("data").innerHTML.split("\n");
     let index = Math.floor(Math.random() * (abilities.length-1));
     console.log(abilities.length);
-    let [name, cd] = abilities[index].split(";");
+    let [name, cooldown] = abilities[index].split(";");
+    solution = Number.parseFloat(cooldown);
+    console.log(solution);
     let image = document.createElement("img");
-    image.src = "icons/" + name + ".png";
+    image.src = "icons/" + name + ".webp";
     document.getElementById("image").append(image);
-    
+    document.getElementById("abilityname").innerText = name;
 }
 
 function confirm() {
     let guess = document.getElementById("input").value;
     if (+guess == guess) {
-        let list = document.querySelectorAll(".guess");
-        list[attempts].innerText = guess;
+        let textfield = document.querySelectorAll(".guess")[attempts];
+        textfield.innerText = guess;
+        let arrow = document.querySelectorAll(".arrow")[attempts];
+        if (guess < solution)
+            arrow.innerText = "↑";
+        if (guess > solution)
+            arrow.innerText = "↓";
         attempts++;
+        if (+guess == solution) {
+            victory();
+            return;
+        }
     }
-    
+    if (attempts == 5) defeat();
+}
+
+function defeat() {
+    endgame();
+    document.getElementById("message").innerText = "Defeat"
+}
+
+function victory() {
+    endgame();
+    document.getElementById("message").innerText = "Victory!"
+}
+
+function endgame() {
+    document.getElementById("confirm").disabled = "true";
+}
+
+function disable() {
+    return;
 }
 
 intialize();
