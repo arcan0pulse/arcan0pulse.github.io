762637763928526e341312aa006721538293fccb
arcan0pulse <niklaslutz09@gmail.com>
2024-03-18 22:00:33 +0100

    restructured website

--- cooldowndle.js
+++ cooldowndle.js
@@ -1,60 +1,90 @@
 let attempts = 0;
-let solution = -1;
-//TODO: Jax, Elise, Jayce, Asol, Yuumi, Nidalee, Kled, Fizz
-function intialize() {
-    let abilities = document.getElementById("data").innerHTML.split("\n");
-    let index = Math.floor(Math.random() * (abilities.length-1));
-    console.log(abilities.length);
-    let [name, cooldown] = abilities[index].split(";");
-    solution = Number.parseFloat(cooldown);
-    console.log(solution);
-    let image = document.createElement("img");
-    image.src = "icons/" + name + ".webp";
-    document.getElementById("image").append(image);
-    document.getElementById("abilityname").innerText = name;
-    document.getElementById("input").addEventListener("keypress", e => enter(e));
-}
+let allowed_attempts = 4;
 
-function confirm(event) {
-    let guess = document.getElementById("input").value;
-    document.getElementById("input").value = "";
-    if (+guess == guess) {
-        let textfield = document.querySelectorAll(".guess")[attempts];
-        textfield.innerText = guess;
-        let arrow = document.querySelectorAll(".arrow")[attempts];
-        if (guess < solution)
-            arrow.innerText = "↑";
-        if (guess > solution)
-            arrow.innerText = "↓";
+import { ABILITIES } from "./abilities.js";
+
+let abilityName;
+let abilityImage;
+let abilityCooldown;
+
+const gameState = JSON.parse(localStorage.getItem("state")) || {
+    gameNumber: -1,
+    guesses: [],
+    hasWon: false,
+  };
+
+const startDay = new Date("03/17/2024");
+const gameNumber = getGameNumber();
+const input = document.getElementById("answer-input");
+const guessbutton = document.getElementById("guess-button");
+
+
+intialize();
+
+//TODO: Jax, Elise, Jayce, Asol, Yuumi, Nidalee, Kled, Fizz, Hwei
+function intialize() {
+    if (gameState.gameNumber !== gameNumber) {
+        gameState.gameNumber = gameNumber;
+        gameState.guesses = [];
+        gameState.hasWon = false;
+        localStorage.setItem("state", JSON.stringify(gameState));
+    }
+    Math.seedrandom(getGameNumber())
+    let i = Math.floor(Math.random(getGameNumber()) * ABILITIES.length);
+    abilityName = ABILITIES[i].split(";")[0];
+    abilityCooldown = ABILITIES[i].split(";")[1];
+    abilityImage = "./icons/" + abilityName + ".webp";
+    document.addEventListener("keypress", e => enter(e));
+    document.getElementById("ability-image").src = abilityImage;
+    document.getElementById("ability-name").innerText = abilityName;
+    guessbutton.onclick = () => confirm();
+}
+ 
+function confirm() {
+    let guess = input.value;
+    console.log(guess);
+    input.value = "";
+    if (guess.length > 0 && !isNaN(guess)) {
+        let guessContainer = document.createElement("div");
+        guessContainer.innerText = guess;
+        guessContainer.classList.add("revealed-answer");
+        let arrowContainer = document.createElement("div");
+        arrowContainer.classList.add("revealed-arrow");
+        arrowContainer.innerText = guess == abilityCooldown ? "✔" : guess < abilityCooldown ? "↑" : "↓";
+        let textfield = document.querySelectorAll(".answer-style")[attempts];
+        textfield.classList.add("flip-out");
+        setTimeout(() => {
+            textfield.append(...[guessContainer, arrowContainer]);
+            guessContainer.classList.add("flip-in");
+            textfield.classList.add("transparent-background");
+        }, 750);
         attempts++;
-        if (+guess == solution) {
-            victory();
+        if (+guess == abilityCooldown) {
+            endGame("Victory");
             return;
         }
     }
-    if (attempts == 5) defeat();
+    if (attempts == allowed_attempts) endGame("Defeat");
+}
+
+function endGame(msg) {
+    let text = document.getElementById("ability-name");
+    text.classList.add("flip-out");
+    setTimeout(() => {
+        text.innerText = msg;
+        text.classList.add("flip-in");
+    }, 750);
+    input.disabled = true;
+    guessbutton.disabled = true;
 }
 
 function enter(event) {
-    if (event.key == "Enter") confirm();
+    if (event.key == "enter") confirm();
 }
 
-function defeat() {
-    endgame();
-    document.getElementById("message").innerText = "Defeat"
+function getGameNumber() {
+    const currDate = new Date();
+    let timeDiff = currDate.getTime() - startDay.getTime();
+    let dayDiff = timeDiff / (1000 * 3600 * 24);
+    return Math.ceil(dayDiff);
 }
-
-function victory() {
-    endgame();
-    document.getElementById("message").innerText = "Victory!"
-}
-
-function endgame() {
-    document.getElementById("confirm").disabled = "true";
-}
-
-function disable() {
-    return;
-}
-
-intialize();
